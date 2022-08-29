0c90b8bfd4d34f709d3f40c9c76a270618896300
mdtauhid204 <mdtauhid204@gmail.com>
2022-08-29 16:47:25 +0530

    complaint

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const ConnectToMongo = require("./moongose");
+
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+var uri =
+  "mongodb+srv://complaint:complaint@cluster0.tiegqro.mongodb.net/complaint?retryWrites=true&w=majority";
+
+ConnectToMongo(uri);
+
+app.use("/auth", require("./routes/auth"));
+app.use("/complaint", require("./routes/complaint"));
+
+app.listen(5000, () => {
+  console.log("server is listiening on server 5000");
+});
